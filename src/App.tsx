30eb5baa9193680c0829219fa081ad526140e31b
Energon715 <TheSlopHead@outlook.com>
2026-09-18 01:15:18 +0300

    chore: make default css file

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import BustScene from './scene/BustScene';
+import BustScene from "./scene/BustScene";
 
 function App() {
   return (
-    <div style={{ width: '100vw', height: '100vh', margin: 0, padding: 0 }}>
+    <div style={{ width: "100vw", height: "100vh", margin: 0, padding: 0 }}>
       <BustScene />
     </div>
   );
